95da13b6bc5cbcbf962c5550596b7a58ff015f1f
MUHAMMAD <muhammadazmirobbani@smkitihsanulfikri.sch.id>
2026-04-07 08:21:59 +0700

    1

--- src/db_proyek2.py
+++ src/db_proyek2.py
@@ -2,3 +2,13 @@ import sqlite3
 
 conn = sqlite3.connect("umkm.db", check_same_thread=False)
 cursor = conn.cursor()
+
+def init_db():
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS produk (
+        nama TEXT PRIMARY KEY,
+        harga INTEGER,
+        stok INTEGER
+    )
+    """)
+    conn.commit()
